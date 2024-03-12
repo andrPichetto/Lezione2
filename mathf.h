bc6f93e6746b08eba0e489b7c667de158d3cd695
Andrea Pichetto <andrepiche28@outlook.it>
2024-03-12 13:03:44 +0100

    Seconda versione - double

--- mathf.h
+++ mathf.h
@@ -5,15 +5,18 @@
 	Details.
 */
 
-#ifndef _MATHFH_
-#define _MATHFH_
+#ifndef MATHF_H 
+#define MATHF_H
 
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h> 
 #include <string.h>
+#include "structs.h"
 
-float Polynomial(float* coeff, int size, float in);
-void Rectangular(float* values, int size, float stepsize, float* integ1, float* integ2);
-float Trapezoidal(float* values, int size, float stepsize);
+double Polynomial(poly_s polyf, double in);
+void Rectangular(double* values, int size, double stepsize, double* integ1, double* integ2);
+double Trapezoidal(double* values, int size, double stepsize);
+
+
 
 #endif
